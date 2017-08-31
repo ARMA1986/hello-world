2a70471c8f428470e32dfeb69d0f6834c03c965d
ARMA1986 <31229383+ARMA1986@users.noreply.github.com>
2017-08-31 00:44:55 -0600

    Add files via upload
    added VS code file

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	string message = "Help Me!?!";
+	
+	// Use this for initialization
+	void Start () {
+		print("What do you need?");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (message);
+	}
+}
+
